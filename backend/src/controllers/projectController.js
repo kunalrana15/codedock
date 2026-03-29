dacd9ea2367cc738088e25d0d814a05724521ecf
kunalrana15 <kunalranabsr9458@gmail.com>
2026-03-29 14:01:13 +0530

    Added the logic to create the new vite project on the backend
    - Using os module of the node
    - Using exec module of the node with promisify

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,13 +1,22 @@
 import util from 'util';
 import child_process from 'child_process';
+import fs from 'fs/promises';
+import uuid4 from 'uuid4';
 
 const execPromisified = util.promisify(child_process.exec);
 
 export const createProjectController = async(req,res) => {
 
-    const { stdout,stderr } = await execPromisified('pwd');
-    console.log('stdout:',stdout);
-    console.log('stderr:',stderr);
+    // Create a unique idea and then insdie the peojects folder create a new folder with that for different users
+    const projectId = uuid4();
+
+    console.log('New Project id is:',projectId);
+    await fs.mkdir(`./projects/${projectId}`);
+
+    // After this call the npm create vite command in the newly folder in the projects folder to intialize react project
+    const response = await execPromisified('npm create vite@latest sandbox -- --template',
+                                           {shell:'powershell.exe',
+                                            cwd: `./projects/${projectId}`});
 
     return res.json({ message: 'Project created' });
     
