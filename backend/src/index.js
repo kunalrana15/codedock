f297153423979a23d66bcf234f3bd220483c189c
kunalrana15 <kunalranabsr9458@gmail.com>
2026-04-13 22:23:41 +0530

    Backend: setting up socket.io in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-
+import { Server } from "socket.io";
 import apiRouter from './routes/index.js';
 import { PORT } from './config/serverConfig.js';
+import { createServer } from "http";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -16,7 +19,13 @@ app.get('/ping',(req,res) => {
     return res.json({message: 'PONG'});
 });
 
-app.listen(PORT,() => {
+
+io.on("connection",(socket) => {
+    console.log("User Connected");
+})
+
+
+server.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
 });
 
