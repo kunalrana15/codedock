59a3cfdc9645604eb309d1fe3abad2ccd6f2909e
kunalrana15 <kunalranabsr9458@gmail.com>
2026-04-14 21:59:49 +0530

    Backend: install the chokidar package which will watch on our files and folder is the project created by user, create the events using socket io for creating,deleting files and folders and writing and updating files

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,18 @@ import { Server } from "socket.io";
 import apiRouter from './routes/index.js';
 import { PORT } from './config/serverConfig.js';
 import { createServer } from "http";
+import chokidar from 'chokidar';
+import path from 'path';
+import { handleEditorSocketEvents } from './sockerHandlers/editorHandler.js';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+        origin: "*",
+        methods: ['GET','POST']
+    }
+});
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -25,6 +33,38 @@ io.on("connection",(socket) => {
 })
 
 
+const editorNamespace = io.of('/editor');
+
+editorNamespace.on('connection',(socket) => {
+   
+    console.log("Editor Connected");
+
+    // somehow we will get the projectId from frontend
+    let projectId = "123";
+
+    if(projectId) {
+        var watcher = chokidar.watch(`./projects/${projectId}`,{
+            ignored: (path) => path.includes("node_modules"),
+            persistent: true, /** Keep the watcher in running state till the time app is running */
+            awaitWriteFinish: {
+                stabilityThreshold: 2000 /** Ensures Stability of files before triggering events */
+            },
+            ignoreInitial: true
+        });
+
+        watcher.on("all",(event,path) => {
+            console.log(event,path);
+        })
+    }
+    
+    handleEditorSocketEvents(socket);
+
+    socket.on("disconnect",async () => {
+        await watcher.close();
+        console.log("Editor Disconnected");
+    })
+});
+ 
 server.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
 });
